51a5fdbdc92064c46bac716c8d3dd26dee804288
Rebecca Bettencourt <beckiergb@gmail.com>
2024-09-05 18:28:16 -0700

    Git something in there

--- test/test.py
+++ test/test.py
@@ -25,16 +25,51 @@ async def test_project(dut):
 
     dut._log.info("Test project behavior")
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    LINE = 0
+    CTRL = 1
+    BMAP = 2
+    ATTR = 4
 
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+    async def sty_write(a, d):
+        # Set address and data
+        dut.ui_in.value = 0xC0 | a
+        dut.uio_in.value = d & 0xFF
+        await ClockCycles(dut.clk, 1)
+        # Enable write
+        dut.ui_in.value = 0x40 | a
+        await ClockCycles(dut.clk, 1)
+        # Disable write
+        dut.ui_in.value = 0xC0 | a
+        await ClockCycles(dut.clk, 1)
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    async def sty_read(a):
+        # Enable output
+        dut.ui_in.value = 0x80 | a
+        await ClockCycles(dut.clk, 1)
+        r1 = dut.uo_out.value & 0xFF
+        r2 = dut.uio_out.value & 0xFF
+        # Disable output
+        dut.ui_in.value = 0xC0 | a
+        await ClockCycles(dut.clk, 1)
+        assert r1 == r2
+        return r1
+
+    async def bmp_write(d):
+        await sty_write(BMAP, d)
+        await sty_write(BMAP + 1, d >> 8)
+
+    async def bmp_read():
+        b1 = await sty_read(BMAP)
+        b2 = await sty_read(BMAP + 1)
+        return b1 | (b2 << 8)
+
+    await sty_write(LINE, 0)
+    await sty_write(CTRL, 0x3C)
+    await bmp_write(0)
+
+    assert await sty_read(LINE) == 0
+    assert await sty_read(CTRL) == 0x3C
+    assert await bmp_read() == 0
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
